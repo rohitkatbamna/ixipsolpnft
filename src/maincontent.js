39825acc511ed2d593d3112fb6641066b6262a0b
rohit katbamna <rohitkatbamna@protonmail.com>
2022-01-21 18:51:27 +0530

    2nd commit

--- src/maincontent.js
+++ src/maincontent.js
@@ -1,68 +1,42 @@
 import React from "react";
-import solCoin from './solanaLogoMark.svg';
 import './mconte.css';
-import ixi from './16275302498922.svg';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDollarSign, faEquals, faPlus } from "@fortawesome/free-solid-svg-icons";
-import Stepone from "./step1";
-import Steptwo from "./step2";
-import Stepthree from "./step3";
+
 
 function Maincontentnft(){
     return(
-        <div className="container-fluid main-conent-nft-all-of-it" style={{backgroundColor:'black'}}>
-            <div className="row">
-                <div className="col-12 text-center text-white" >
-                    <p style={{fontSize:'80px'}} className="fw-bold">Salutation And Greeting to Crypto enthusiast  </p>
-                    <p style={{fontSize:'50px'}} className="fw-regular">You are cordially invited to make Your NFT</p>
+        <div className="card main-content-nft-all-of-it" style={{backgroundColor:'#1C1A1C'}}>
+            <div className="card-body container-fluid">
+                <div className="row justify-content-around">
+                    <div className="card col-5" style={{borderRadius:'16px'}}>
+                        <div className="card-body text-center">
+                            <h3 className="fw-bold">Account ID</h3>
+                            <h1>1234567890</h1>
+                            <h3 className="mt-5 fw-bold">Account Balance</h3>
+                            <h1>1234567890</h1>
+                        </div>
+                    </div>
+                    <div className="card col-5" style={{borderRadius:'16px'}}>
+                        <div className="card-body text-center">
+                            <h3 className="fw-bold">Account ID</h3>
+                            <h1>1234567890</h1>
+                            <h3 className="mt-5 fw-bold">Account Balance</h3>
+                            <h1>1234567890</h1>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="card col-12 text-center text-white" style={{backgroundColor:'black'}}>
-                    <div className="card-body">
-                        <div>
-                            <h1>Have A piece of art And want to make money hell if you don't have art but still want to make money by selling scribbly line you drawn together 
-                            </h1>
-                            <h1>WE GOT YOU COVERED</h1>
-                            <h1>Just Follow The Steps below</h1>
+                <div className="row" style={{marginTop:'100px'}}>
+                    <div className="col-xl-5 ms-auto me-auto col-md-8">
+                        <div className="card" style={{borderRadius:'16px'}}>
+                            <div className="card-body">
+                                <div className="d-flex justify-content-around">
+                                    <input type='text' placeholder='Enter Tokens To Transfer'style={{borderRadius:'8px',border:'2px solid black'}}/>
+                                    <button type="button" class="btn btn-dark fs-3" style={{borderRadius:'16px'}}>Transfer</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="row mt-5">
-                <div className="col-12">
-                    <Stepone />
-                </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-12">
-                    <Steptwo />
-                </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-12">
-                    <Stepthree />
-                </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-12 text-center text-white fs-1 fw-bold">
-                    In Summary  
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12 justify-content-around align-items-center d-flex flex-wrap mb-5 mt-3" style={{border:'4px solid white',borderRadius:'16px'}}>
-                    <img src={solCoin} style={{width:'50px'}} className="img-make-rotate" alt="Sol coin "/>
-                    <FontAwesomeIcon icon={faPlus} size="6x" style={{color:'white'}}/>
-                    <h1 className="text-white">NFT</h1>
-                    <FontAwesomeIcon icon={faPlus} size="6x" style={{color:'white'}}/>
-                    <img src={ixi}  alt="ixi symbol"/>
-                    <FontAwesomeIcon icon={faEquals} size="6x" style={{color:'white'}} />
-                    <FontAwesomeIcon icon={faDollarSign}  size="6x" style={{color:'white'}}/>
-                    <FontAwesomeIcon icon={faDollarSign}  size="6x" style={{color:'white'}}/>
-                    <FontAwesomeIcon icon={faDollarSign}  size="6x" style={{color:'white'}}/>
-                </div>
-            </div>
-
         </div>
     );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -1,52 +1,12 @@
 import React from "react";
-import logonavbarixi from './solanaLogoMark.svg';
 import './navbarcss.css'; 
-import ixi from './16275302498922.svg';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 
 function Navbarfornft(){
     return(
-        <div className="card sticky-top shadow navbar-of-all-of-it-font-change" style={{backgroundColor:'black',fontFamily:'cursive'}}>
-            <div className="card-body">
-                <nav className="navbar navbar-expand-lg navbar navbar-dark">
-                    <div className="container-fluid">
-                        <a className="navbar-brand d-flex justify-content-around align-items-center" href="#">
-                           <img src={ixi} style={{width:'50px'}}/> <FontAwesomeIcon icon={faPlus} size="1x"/> <img src={logonavbarixi} style={{width:'25px',marginLeft:'5px',marginRight:'5px'}} /> <FontAwesomeIcon icon={faPlus} size="1x"/><h4>NFT</h4> 
-                        </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link" href="#">Validation</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Developers
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Getting Started</a></li>
-                                <li><a className="dropdown-item" href="#">NFTs</a></li>
-                                <li><hr className="dropdown-divider"/></li>
-                                <li><a className="dropdown-item" href="#">Community</a></li>
-                            </ul>
-                            </li>
-                            <li className="nav-item">
-                            <a className="nav-link">Ecosystem</a>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="So it comes to this" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                        </div>
-                    </div>
-                </nav>
+        <div className="card shadow navbar-of-all-of-it-font-change" style={{backgroundColor:'#1C1A1C'}}>
+            <div className="card-body text-left ms-auto">
+                <button type="button" class="btn btn-light me-5 fs-3" style={{height:'100px',borderRadius:'16px'}}>Connect To Wallet</button>
             </div>
         </div>
     );
